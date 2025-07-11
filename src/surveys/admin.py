03d6bd72001004032997cb1781a5087c5b3220c5
Bharat Kumar <kumar.bhart28@gmail.com>
2025-07-11 23:46:33 +0530

    Revert "Release: Merge develop into main (Initial Release v1.0.0) (#8)" (#9)
    This reverts commit 38809ee85f23ee4421980d8a7c2687186f263aa1.

--- src/surveys/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from surveys.models.answer import Answer
-from surveys.models.question import Question
-from surveys.models.survey import Survey
-
-admin.site.register(Survey)
-admin.site.register(Question)
-admin.site.register(Answer)
